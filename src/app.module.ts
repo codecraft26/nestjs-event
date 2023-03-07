f680ba58873c4acc6e9039e6bacf1befba80ec99
Aman Kumar Gupta <75511608+codecraft26@users.noreply.github.com>
2023-03-07 23:34:14 +0530

    changes in main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
+  
+  //imprted module
   imports: [EventEmitterModule.forRoot(),ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
